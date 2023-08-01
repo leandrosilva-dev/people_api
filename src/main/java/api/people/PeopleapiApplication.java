6d6d52dabe1188f082f24e965dc031c722fddbd3
Leandro Siqueira <android.leandro.siqueira@gmail.com>
2023-07-31 23:11:20 -0300

    Make the project run in localhost

---
+++ src/main/java/api/people/PeopleapiApplication.java
@@ -0,0 +1,14 @@
+package api.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleapiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("TEST");
+		SpringApplication.run(PeopleapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/people/PeopleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
